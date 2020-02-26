cc987fa1687828a07355e720557c6e63b778553b
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-02-26 13:49:17 -0800

    update to add buildspec for aws codebuild

--- codebuild.sh
+++ codebuild.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "It works!";
+###
+### DEPRECATED!!!
+###
+echo "It works!"
+CONTAINER_NAME=739750115356.dkr.ecr.us-east-2.amazonaws.com/containerized-status-node
+
+docker build -t $CONTAINER_NAME .
+
+docker push $CONTAINER_NAME:latest
 
 exit 0;
